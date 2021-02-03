07caf0a4d4f19706461ec0b9258a82ce392e9802
Andrew <konoplyanik0504@gmail.com>
2021-02-04 00:22:24 +0300

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exception = new Exception[5];
+            exception[0] = new OverflowException();
+            exception[1] = new FormatException();
+            exception[2] = new ArgumentException();
+            exception[3] = new RankException();
+            exception[4] = new MyException();
+
+            for (int i = 0; i < exception.Length; i++)
+            {
+                try
+                {
+                    throw exception[i];
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(exception[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEnteredEvent += SortingSurname;
+
+            try
+            {
+                numberReader.Read();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено некорректное значение");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void SortingSurname(int number)
+        {
+            var surnameList = new List<string>();
+
+            surnameList.Add("Петров");
+            surnameList.Add("Янковский");
+            surnameList.Add("Иванов");
+            surnameList.Add("Агутин");
+            surnameList.Add("Кононов");
+
+            switch (number)
+            {
+                case 1:
+                    surnameList.Sort();
+
+                    foreach (var item in surnameList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+
+                case 2:
+                    surnameList.Sort();
+                    surnameList.Reverse();
+
+                    foreach (var item in surnameList)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+            }
+        }
+    }
+
+    class NumberReader
+    {
+        public delegate void NumberEnteredDelegate(int number);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите число 1 (сортировка А-Я), либо число 2 (сортировка Я-А)");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2) throw new FormatException();
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
+        }
+    }
+}
